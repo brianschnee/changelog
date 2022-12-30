6d5f743c091e6e44936d095b4c9278ab30c68f0b
brianschnee <brian.a.schnee@gmail.com>
2022-12-30 13:04:43 -0500

    Feat: express router and routes

---
+++ src/router.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express'
+
+const router = Router()
+
+// Product
+router.get('/product', (req, res) => {
+   res.status(200).json({ message: 'success' })
+})
+router.get('/product/:id', () => {})
+router.put('/product/:id', () => {})
+router.post('/product', () => {})
+router.delete('/product/:id', () => {})
+
+// Update
+router.get('/update', () => {})
+router.get('/update/:id', () => {})
+router.put('/update/:id', () => {})
+router.post('/update', () => {})
+router.delete('/update/:id', () => {})
+
+// UpdatePoint
+router.get('/updatepoint', () => {})
+router.get('/updatepoint/:id', () => {})
+router.put('/updatepoint/:id', () => {})
+router.post('/updatepoint', () => {})
+router.delete('/updatepoint/:id', () => {})
+
+export default router
