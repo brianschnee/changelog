019c0b1557264c97dfd163c068bf8c419e2b99d1
brianschnee <brian.a.schnee@gmail.com>
2022-12-30 13:04:18 -0500

    Feat: middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import router from './router'
+import morgan from 'morgan'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-    res.status(200).json({ message: "success" })
+   res.status(200).json({ message: 'success' })
 })
+
+app.use('/api', router)
 
 export default app
