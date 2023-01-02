0a7fabbfc131d5bf50dfc3543108ed263de7146a
brianschnee <brian.a.schnee@gmail.com>
2023-01-02 18:32:17 -0500

    Feat: hashPassword and comparePasswords functions

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,11 +1,21 @@
 import { User } from '@prisma/client'
 import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
-const JWT_SECRET = process.env.JWT_SECRET ?? 'cookie'
+export const comparePasswords = (password: string, hash: string) => {
+   return bcrypt.compare(password, hash)
+}
+
+export const hashPassword = (password: string) => {
+   return bcrypt.hash(password, 10)
+}
 
 export const createJWT = (user: User) => {
-   const token = jwt.sign(user.id, JWT_SECRET)
+   const token = jwt.sign(
+      { id: user.id, username: user.username },
+      process.env.JWT_SECRET!
+   )
    return token
 }
 
@@ -23,11 +33,12 @@ export const authenticate = (
    if (!token) return res.status(401).json({ message: 'Not authorized' })
 
    try {
-      const user = jwt.verify(token, JWT_SECRET) as Pick<
+      const user = jwt.verify(token, process.env.JWT_SECRET!) as Pick<
          User,
          'id' | 'username'
       >
       req.user = user
+
       next()
    } catch (e) {
       console.error(e)
