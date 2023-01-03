cf3b8f545df36319a79fa8ea3324f22a85f28d5d
brianschnee <brian.a.schnee@gmail.com>
2023-01-03 15:38:56 -0500

    Fix: abstract rules for validation middleware

--- src/router.ts
+++ src/router.ts
@@ -1,72 +1,41 @@
 import { Router } from 'express'
-import { body } from 'express-validator'
+import {
+   bulletPointRules,
+   productRules,
+   updateBulletPointRules,
+   updateRules,
+   updateUpdateRules,
+   validate
+} from './utils/middleware'
 import {
    createProduct,
    deleteProduct,
    getOneProduct,
    getProducts,
+   updateProduct
 } from './handlers/product'
-import { validateReqBody } from './utils/middleware'
 
 const router = Router()
 
 // Product
 router.get('/product', getProducts)
-// body('name') <- req.body should have a field on it called 'name'
-router.get('/product/:id', body('name'), getOneProduct)
-router.put(
-   '/product/:id',
-   body('name').exists().isString(),
-   validateReqBody,
-   (req, res) => {}
-)
-router.post(
-   '/product',
-   body('name').exists().isString(),
-   validateReqBody,
-   createProduct
-)
+router.get('/product/:id', productRules(), validate, getOneProduct)
+router.put('/product/:id', productRules(), validate, updateProduct)
+router.post('/product', productRules(), validate, createProduct)
 router.delete('/product/:id', deleteProduct)
 
 // Update
 router.get('/update', () => {})
 router.get('/update/:id', () => {})
-router.put(
-   '/update/:id',
-   body('title').optional(),
-   body('body').optional(),
-   body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
-   body('version').optional(),
-   validateReqBody,
-   (req, res) => {}
-)
-router.post(
-   '/update',
-   body('title').exists().isString(),
-   body('body').exists().isString(),
-   validateReqBody,
-   (req, res) => {}
-)
+router.put('/update/:id', updateUpdateRules(), validate, () => {})
+router.post('/update', updateRules(), validate, () => {})
 router.delete('/update/:id', () => {})
 
-// UpdatePoint
-router.get('/updatepoint', () => {})
-router.get('/updatepoint/:id', () => {})
-router.put(
-   '/updatepoint/:id',
-   body('name').optional().isString(),
-   body('description').optional().isString(),
-   validateReqBody,
-   () => {}
-)
-router.post(
-   '/updatepoint',
-   body('name').exists().isString(),
-   body('description').exists().isString(),
-   body('updateId').exists().isString(),
-   validateReqBody,
-   () => {}
-)
-router.delete('/updatepoint/:id', () => {})
+// BulletPoint
+router.get('/bulletpoint', () => {})
+router.get('/bulletpoint/:id', () => {})
+router.put('/bulletpoint/:id', updateBulletPointRules(), validate, () => {})
+router.post('/bulletpoint', bulletPointRules(), validate, () => {})
+router.delete('/bulletpoint/:id', () => {})
 
 export default router
--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -1,7 +1,41 @@
 import { RequestHandler } from 'express'
-import { validationResult } from 'express-validator'
+import { body, validationResult } from 'express-validator'
 
-export const validateReqBody: RequestHandler = (req, res, next) => {
+// rules for properties existing on request object
+export const productRules = () => {
+   return body('name').exists().isString()
+}
+
+export const updateRules = () => {
+   return [body('title').exists().isString(), body('body').exists().isString()]
+}
+
+export const updateUpdateRules = () => {
+   return [
+      body('title').optional(),
+      body('body').optional(),
+      body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
+      body('version').optional()
+   ]
+}
+
+export const bulletPointRules = () => {
+   return [
+      body('name').exists().isString(),
+      body('description').exists().isString(),
+      body('updateId').exists().isString()
+   ]
+}
+
+export const updateBulletPointRules = () => {
+   return [
+      body('name').optional().isString(),
+      body('description').optional().isString()
+   ]
+}
+
+// if any rules werent met before reaching the handler, display errors
+export const validate: RequestHandler = (req, res, next) => {
    const errors = validationResult(req)
 
    if (!errors.isEmpty()) {
