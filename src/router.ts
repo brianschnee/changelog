828f131ef2d013cfe1097ec939108f8104f58ff8
brianschnee <brian.a.schnee@gmail.com>
2023-01-07 17:05:19 -0500

    Feat: use update handlers

--- src/router.ts
+++ src/router.ts
@@ -14,22 +14,29 @@ import {
    getProducts,
    updateProduct
 } from './handlers/product'
+import {
+   createUpdate,
+   deleteUpdate,
+   getOneUpdate,
+   getUpdates,
+   updateUpdate
+} from './handlers/update'
 
 const router = Router()
 
 // Product
 router.get('/product', getProducts)
-router.get('/product/:id', productRules(), validate, getOneProduct)
+router.get('/product/:id', getOneProduct)
 router.put('/product/:id', productRules(), validate, updateProduct)
 router.post('/product', productRules(), validate, createProduct)
 router.delete('/product/:id', deleteProduct)
 
 // Update
-router.get('/update', () => {})
-router.get('/update/:id', () => {})
-router.put('/update/:id', updateUpdateRules(), validate, () => {})
-router.post('/update', updateRules(), validate, () => {})
-router.delete('/update/:id', () => {})
+router.get('/update', getUpdates)
+router.get('/update/:id', getOneUpdate)
+router.put('/update/:id', updateUpdateRules(), validate, updateUpdate)
+router.post('/update', updateRules(), validate, createUpdate)
+router.delete('/update/:id', deleteUpdate)
 
 // BulletPoint
 router.get('/bulletpoint', () => {})
